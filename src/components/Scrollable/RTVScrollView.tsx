c18160b31f574b20b6e16c17157a0ee2424ca6d5
adithyavis <adithyaviswam@gmail.com>
2026-02-23 00:13:32 +0530

    feat: fix double ref forward type

--- src/components/Scrollable/RTVScrollView.tsx
+++ src/components/Scrollable/RTVScrollView.tsx
@@ -14,145 +14,148 @@ import { useSyncScrollWithPanTranslation } from '../../hooks/scrollable/useSyncS
 import { SHOULD_RENDER_ABSOLUTE_HEADER } from '../../constants/scrollable';
 
 export const RTVScrollViewWithoutScrollHandler = React.memo(
-  forwardRef<
-    React.ForwardedRef<Animated.ScrollView>,
-    React.ComponentProps<typeof ScrollView>
-  >((props, ref) => {
-    //#region props
-    const { children, ...rest } = props;
-    //#endregion
+  forwardRef<Animated.ScrollView, React.ComponentProps<typeof ScrollView>>(
+    (props, ref) => {
+      //#region props
+      const { children, ...rest } = props;
+      //#endregion
 
-    //#region context
-    const { animatedTranslateYSV } = useHeaderContext();
+      //#region context
+      const { animatedTranslateYSV } = useHeaderContext();
 
-    const { tabViewHeaderLayout, tabBarLayout, tabViewCarouselLayout } =
-      useInternalContext();
+      const { tabViewHeaderLayout, tabBarLayout, tabViewCarouselLayout } =
+        useInternalContext();
 
-    //#endregion
+      //#endregion
 
-    //#region variables
-    const scrollRef = useAnimatedRef<Animated.ScrollView>();
+      //#region variables
+      const scrollRef = useAnimatedRef<Animated.ScrollView>();
 
-    const scrollGesture = useMemo(
-      () =>
-        Gesture.Native()
-          .shouldCancelWhenOutside(false)
-          .disallowInterruption(true),
-      []
-    );
-    //#endregion
+      const scrollGesture = useMemo(
+        () =>
+          Gesture.Native()
+            .shouldCancelWhenOutside(false)
+            .disallowInterruption(true),
+        []
+      );
+      //#endregion
 
-    //#region styles
-    const animatedContentContainerStyle = useAnimatedStyle(() => {
-      return {
-        transform: [{ translateY: animatedTranslateYSV.value }],
-      };
-    }, [animatedTranslateYSV]);
+      //#region styles
+      const animatedContentContainerStyle = useAnimatedStyle(() => {
+        return {
+          transform: [{ translateY: animatedTranslateYSV.value }],
+        };
+      }, [animatedTranslateYSV]);
 
-    const translatingContentContainerStyle = useMemo(() => {
-      return [
+      const translatingContentContainerStyle = useMemo(() => {
+        return [
+          animatedContentContainerStyle,
+          {
+            paddingBottom: tabViewHeaderLayout.height,
+            minHeight:
+              tabViewCarouselLayout.height + tabViewHeaderLayout.height,
+          },
+        ];
+      }, [
         animatedContentContainerStyle,
-        {
-          paddingBottom: tabViewHeaderLayout.height,
+        tabViewCarouselLayout.height,
+        tabViewHeaderLayout.height,
+      ]);
+
+      const nonTranslatingContentContainerStyle = useMemo(() => {
+        return {
+          paddingTop: tabBarLayout.height + tabViewHeaderLayout.height,
           minHeight: tabViewCarouselLayout.height + tabViewHeaderLayout.height,
-        },
-      ];
-    }, [
-      animatedContentContainerStyle,
-      tabViewCarouselLayout.height,
-      tabViewHeaderLayout.height,
-    ]);
+        };
+      }, [
+        tabBarLayout.height,
+        tabViewCarouselLayout.height,
+        tabViewHeaderLayout.height,
+      ]);
+      //#endregion
 
-    const nonTranslatingContentContainerStyle = useMemo(() => {
-      return {
-        paddingTop: tabBarLayout.height + tabViewHeaderLayout.height,
-        minHeight: tabViewCarouselLayout.height + tabViewHeaderLayout.height,
-      };
-    }, [
-      tabBarLayout.height,
-      tabViewCarouselLayout.height,
-      tabViewHeaderLayout.height,
-    ]);
-    //#endregion
+      //#region hooks
+      useImperativeHandle(ref, () => scrollRef.current as any);
 
-    //#region hooks
-    useImperativeHandle(ref, () => scrollRef.current as any);
+      useSyncScrollWithPanTranslation(scrollRef);
+      //#endregion
 
-    useSyncScrollWithPanTranslation(scrollRef);
-    //#endregion
-
-    //#region render
-    return (
-      <GestureDetector gesture={scrollGesture}>
-        <Animated.ScrollView ref={scrollRef} {...rest} scrollEventThrottle={16}>
-          {SHOULD_RENDER_ABSOLUTE_HEADER ? (
-            <Animated.View
-              style={[
-                styles.contentContainer,
-                nonTranslatingContentContainerStyle,
-              ]}
-            >
-              {children}
-            </Animated.View>
-          ) : (
-            <Animated.View
-              style={[
-                styles.contentContainer,
-                translatingContentContainerStyle,
-              ]}
-            >
-              {children}
-            </Animated.View>
-          )}
-        </Animated.ScrollView>
-      </GestureDetector>
-    );
-    //#endregion
-  })
+      //#region render
+      return (
+        <GestureDetector gesture={scrollGesture}>
+          <Animated.ScrollView
+            ref={scrollRef}
+            {...rest}
+            scrollEventThrottle={16}
+          >
+            {SHOULD_RENDER_ABSOLUTE_HEADER ? (
+              <Animated.View
+                style={[
+                  styles.contentContainer,
+                  nonTranslatingContentContainerStyle,
+                ]}
+              >
+                {children}
+              </Animated.View>
+            ) : (
+              <Animated.View
+                style={[
+                  styles.contentContainer,
+                  translatingContentContainerStyle,
+                ]}
+              >
+                {children}
+              </Animated.View>
+            )}
+          </Animated.ScrollView>
+        </GestureDetector>
+      );
+      //#endregion
+    }
+  )
 );
 
 export const RTVScrollView = React.memo(
-  forwardRef<
-    React.ForwardedRef<Animated.ScrollView>,
-    React.ComponentProps<typeof ScrollView>
-  >((props, ref) => {
-    //#region props
-    const {
-      onScroll,
-      onScrollEndDrag,
-      onScrollBeginDrag,
-      onMomentumScrollEnd,
-      onMomentumScrollBegin,
-      ...rest
-    } = props;
-    //#endregion
+  forwardRef<Animated.ScrollView, React.ComponentProps<typeof ScrollView>>(
+    (props, ref) => {
+      //#region props
+      const {
+        onScroll,
+        onScrollEndDrag,
+        onScrollBeginDrag,
+        onMomentumScrollEnd,
+        onMomentumScrollBegin,
+        ...rest
+      } = props;
+      //#endregion
 
-    //#region variables
-    const scrollRef = useAnimatedRef<Animated.ScrollView>();
+      //#region variables
+      const scrollRef = useAnimatedRef<Animated.ScrollView>();
 
-    const handleScroll = useScrollHandlers({
-      onScroll,
-      onScrollEndDrag,
-      onScrollBeginDrag,
-      onMomentumScrollEnd,
-      onMomentumScrollBegin,
-    });
-    //#endregion
+      const handleScroll = useScrollHandlers({
+        onScroll,
+        onScrollEndDrag,
+        onScrollBeginDrag,
+        onMomentumScrollEnd,
+        onMomentumScrollBegin,
+      });
+      //#endregion
 
-    //#region hooks
-    useImperativeHandle(ref, () => scrollRef.current as any);
-    //#endregion
+      //#region hooks
+      useImperativeHandle(ref, () => scrollRef.current as any);
+      //#endregion
 
-    //#region render
-    return (
-      <RTVScrollViewWithoutScrollHandler
-        {...rest}
-        onScroll={handleScroll}
-        ref={ref}
-      />
-    );
-    //#endregion
-  })
+      //#region render
+      return (
+        <RTVScrollViewWithoutScrollHandler
+          {...rest}
+          onScroll={handleScroll}
+          ref={scrollRef}
+        />
+      );
+      //#endregion
+    }
+  )
 );
 
 const styles = StyleSheet.create({
